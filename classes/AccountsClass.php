23c0279070918885c24aeee900930d5dda8331b3
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-02-01 19:40:37 +0100

    Added Accounts class.

---
+++ classes/AccountsClass.php
@@ -0,0 +1,15 @@
+<?php
+
+//namespace classes;
+
+require '../vendor/autoload.php';
+
+class Accounts
+{
+    private $database;
+
+    public function __construct(MySQL $mySQL)
+    {
+        $this->database = $mySQL->sendPDO();
+    }
+}
