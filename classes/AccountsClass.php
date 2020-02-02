db3dec2fe5a64451a231ce56ae20bdc40b9c2f87
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-02-02 12:27:08 +0100

    Added getAccount & getAllAccounts()

--- classes/AccountsClass.php
+++ classes/AccountsClass.php
@@ -1,13 +1,40 @@
 <?php
 
 require '../vendor/autoload.php';
+include 'MySqlClass.php';
 
 class Accounts
 {
-    private $database;
+    private $db;
 
     public function __construct(MySQL $mySQL)
     {
-        $this->database = $mySQL->sendPDO();
+        $this->db = $mySQL->connect();
+    }
+    public function getAllAccounts()
+    {
+        $sql = "SELECT * FROM account";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        return $results;
+    }
+    public function getAccount($id)
+    {
+        $sql = "SELECT * FROM account WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        return $result;
     }
 }
+
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+//$dotenv->load();
+
+//$mySQL = new MySQL();
+//$accounts = new Accounts($mySQL);
+
+//print_r($accounts->getAllAccounts());
+//print_r($accounts->getAccount(2));
