9ee387d36ef088077ddc7f72eaa7a371557ba6f9
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-02-02 12:32:26 +0100

    Added getAllTransactions() & getTransaction()

--- classes/TransactionsClass.php
+++ classes/TransactionsClass.php
@@ -1,13 +1,40 @@
 <?php
 
 require '../vendor/autoload.php';
+include 'MySqlClass.php';
 
 class Transactions
 {
-    private $pdo;
+    private $db;
 
     public function __construct(MySQL $mySQL)
     {
-        $tihs->pdo = $mySQL->sendPDO();
+        $this->db = $mySQL->connect();
+    }
+    public function getAllTransactions()
+    {
+        $sql = "SELECT * FROM transactions";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        return $results;
+    }
+    public function getTransaction($id)
+    {
+        $sql = "SELECT * FROM transactions WHERE transaction_id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        return $result;
     }
 }
+
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+//$dotenv->load();
+
+//$mySQL = new MySQL();
+//$transaction = new Transactions($mySQL);
+
+//print_r($transaction->getAllTransactions());
+//print_r($transaction->getTransaction(175));
