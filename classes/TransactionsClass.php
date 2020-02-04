653899288ba195860e6b9c4e2e3a363608443b1b
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-02-04 15:03:16 +0100

    Catch if user's balance is less than what they're sending. Catch if user's transfering zero or less amount.

--- classes/TransactionsClass.php
+++ classes/TransactionsClass.php
@@ -3,7 +3,7 @@
 require '../vendor/autoload.php';
 include 'MySqlClass.php';
 
-class Transactions
+class Transactions extends Users
 {
     private $db;
 
@@ -45,21 +45,36 @@ class Transactions
     public function makeTransaction($data)
     {
         try {
-            $sql = "INSERT INTO transactions(from_amount, from_account, from_currency,
-                   to_amount, to_account, to_currency, currency_rate, date)
-                   VALUES (:from_amount, :from_account, :from_currency,
-                   :to_amount, :to_account, :to_currency, :currency_rate, NOW())";
-            $stmt = $this->db->prepare($sql);
-            $stmt->bindParam(':from_amount', $data['from_amount'], FILTER_SANITIZE_NUMBER_INT);
-            $stmt->bindParam(':from_account', $data['from_account'], FILTER_SANITIZE_NUMBER_INT);
-            $stmt->bindParam(':from_currency', $data['from_currency'], FILTER_SANITIZE_STRING);
-            $stmt->bindParam(':to_amount', $data['to_amount'], FILTER_SANITIZE_NUMBER_INT);
-            $stmt->bindParam(':to_account', $data['to_account'], FILTER_SANITIZE_NUMBER_INT);
-            $stmt->bindParam(':to_currency', $data['to_currency'], FILTER_SANITIZE_STRING);
-            $stmt->bindParam(':currency_rate', $data['currency_rate'], FILTER_SANITIZE_NUMBER_FLOAT);
-            $stmt->execute();
+            $getUser = new Users($db);
+            $result = $getUser->getUser($data['account_id']);
+            if ($result == false) {
+                throw new \Exception("Could not get user");
+            }
+            try {
+                $sql = "INSERT INTO transactions(from_amount, from_account, from_currency,
+                       to_amount, to_account, to_currency, currency_rate, date)
+                       VALUES (:from_amount, :from_account, :from_currency,
+                       :to_amount, :to_account, :to_currency, :currency_rate, NOW())";
+                $stmt = $this->db->prepare($sql);
+                $sendAmount = $stmt->bindParam(':from_amount', $data['from_amount'], FILTER_SANITIZE_NUMBER_INT);
+                $stmt->bindParam(':from_account', $data['from_account'], FILTER_SANITIZE_NUMBER_INT);
+                $stmt->bindParam(':from_currency', $data['from_currency'], FILTER_SANITIZE_STRING);
+                $stmt->bindParam(':to_amount', $data['to_amount'], FILTER_SANITIZE_NUMBER_INT);
+                $stmt->bindParam(':to_account', $data['to_account'], FILTER_SANITIZE_NUMBER_INT);
+                $stmt->bindParam(':to_currency', $data['to_currency'], FILTER_SANITIZE_STRING);
+                $stmt->bindParam(':currency_rate', $data['currency_rate'], FILTER_SANITIZE_NUMBER_FLOAT);
+                if ($sendAmount <= 0) {
+                    throw new \Exception("The amount sent is less or equal zero.");
+                }
+                if ($sendAmount > $result['balance']) {
+                    throw new \Exception("User's balance is to low to make transaction.");
+                }
+                $stmt->execute();
+            } catch (\Exception $e) {
+                echo "Transaction failed: " . $e->getMessage();
+            }
         } catch (\Exception $e) {
-            echo "Transaction failed: " . $e->getMessage();
+            echo "Error: " . $e->getMessage();
         }
     }
 }
