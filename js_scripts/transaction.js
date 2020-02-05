2bf97ef5f1f68f1e26c8bdb7fb8e12941f63103e
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-02-05 13:15:52 +0100

    Made the transaction js get api for currency rate for from_currency

--- js_scripts/transaction.js
+++ js_scripts/transaction.js
@@ -1,14 +1,39 @@
 $(document).ready(function() {
     $("#transaction").on("submit", function(e) {
+        e.preventDefault();
         let form = $("#transaction").serializeArray();
-        $.ajax({
-            type: "post",
-            url: "http://localhost/Bank-application/api/makeTransaction.php",
-            data: form,
-            success: function(data) {},
-            error: function(error) {
-                console.log(error);
-            }
-        });
+        let from_currency = form[1].value;
+        getCurrency();
+
+        function getCurrency() {
+            $.ajax({
+                type:'get',
+                url: "https://api.exchangeratesapi.io/latest?base="+from_currency+"&symbols=USD",
+                dataType: "json",
+                success: function(data){
+                    console.log(data);
+                    postTransaction();
+                },
+                error: function(request,status) {
+                    console.log(request);
+                    console.log(status);
+                }
+            });
+        }
+
+        function postTransaction() {
+            $.ajax({
+                type: "post",
+                url: "http://localhost/Bank-application/api/makeTransaction.php",
+                data: form,
+                success: function(data) {
+                    console.log(form);
+                },
+                error: function(request, status) {
+                    console.log(request);
+                    console.log(status);
+                }
+            });
+        }
     });
 });
