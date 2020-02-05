3b0b4067afb4c4ae3a74256f0bf2b192e599778a
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-02-05 10:14:38 +0100

    fixed to_amount if the currencies are the same.

--- classes/TransactionsClass.php
+++ classes/TransactionsClass.php
@@ -71,10 +71,14 @@ class Transactions
                     $stmt->bindParam(':from_amount', $data['from_amount'], FILTER_SANITIZE_NUMBER_INT);
                     $stmt->bindParam(':from_account', $data['from_account'], FILTER_SANITIZE_NUMBER_INT);
                     $stmt->bindParam(':from_currency', $data['from_currency'], FILTER_SANITIZE_STRING);
-                    $stmt->bindParam(':to_amount', $data['to_amount'], FILTER_SANITIZE_NUMBER_INT);
                     $stmt->bindParam(':to_account', $data['to_account'], FILTER_SANITIZE_NUMBER_INT);
                     $stmt->bindParam(':to_currency', $data['to_currency'], FILTER_SANITIZE_STRING);
                     $stmt->bindParam(':currency_rate', $data['currency_rate'], FILTER_SANITIZE_NUMBER_FLOAT);
+                    if ($data['from_currency'] == $data['to_currency']) {
+                        $stmt->bindParam(':to_amount', $data['from_amount'], FILTER_SANITIZE_NUMBER_INT);
+                    } else {
+                        $stmt->bindParam(':to_amount', $data['to_amount'], FILTER_SANITIZE_NUMBER_INT);
+                    }
                     if ($data['from_amount'] <= 0) {
                         throw new \Exception("The amount sent is less or equal zero.");
                     }
